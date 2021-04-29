81b58d1fb8a7e89cf29e36fccb910b8aba4e1a21
Iain A Maxwell <straightedgescotsman@gmail.com>
2021-04-29 13:24:18 +0100

    User can add ingredients to recipes

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputReference: ElementRef;
+  @ViewChild('amountInput') amountInputReference: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem() {
+    const name = this.nameInputReference.nativeElement.value;
+    const amount = this.amountInputReference.nativeElement.value;
+
+    const newIngredient = new Ingredient(name, amount)
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
