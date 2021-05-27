43189ec39bf19ce4df32f2872781ec843e7bb3c0
Iain A Maxwell <straightedgescotsman@gmail.com>
2021-05-27 15:05:01 +0100

    Moving from EventEmitters to subjects for casading field values

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -26,7 +25,6 @@ export class RecipeService {
   ];
 
   constructor(private shoppingListService: ShoppingListService) {
-
   };
 
   getRecipes() {
